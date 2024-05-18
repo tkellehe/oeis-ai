c3b9dd512d25c2399630fd6e8137c87afe522663
tkellehe <mitchell.kelleher@gmail.com>
2024-05-18 14:29:44 -0400

    Update model.js

--- v1/model.js
+++ v1/model.js
@@ -1,9 +1,18 @@
+const tf = require('@tensorflow/tfjs');
+
 var modelv1 = {}
 window['modelv1'] = modelv1
 
+// Load the tokenizer
 fetch('v1/tokenizer.json')
   .then(response => response.json())
   .then(data => {
     modelv1.tokenizerJson = data
   })
   .catch(error => console.error('Error:', error));
+
+// Load the TF model
+async function loadModel() {
+    modelv1.model = await tf.loadLayersModel('v1/model.json');
+}
+loadModel().catch(console.error);
