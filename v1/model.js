bea356aa1fed1cfea2f522c9ba3c709306b01c15
tkellehe <mitchell.kelleher@gmail.com>
2024-05-18 14:23:19 -0400

    Update model.js

--- v1/model.js
+++ v1/model.js
@@ -4,6 +4,6 @@ window['modelv1'] = modelv1
 fetch('v1/tokenizer.json')
   .then(response => response.json())
   .then(data => {
-    modelv1.tokenizer = data
+    modelv1.tokenizerJson = data
   })
   .catch(error => console.error('Error:', error));
