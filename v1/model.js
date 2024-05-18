73c09f869c1903c50c03b20b8566c19ee4c85c88
tkellehe <mitchell.kelleher@gmail.com>
2024-05-18 14:23:00 -0400

    Update model.js

--- v1/model.js
+++ v1/model.js
@@ -1,3 +1,9 @@
-window['modelv1'] = {
+var modelv1 = {}
+window['modelv1'] = modelv1
 
-}
+fetch('v1/tokenizer.json')
+  .then(response => response.json())
+  .then(data => {
+    modelv1.tokenizer = data
+  })
+  .catch(error => console.error('Error:', error));
